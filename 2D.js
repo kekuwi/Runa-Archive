e0ab2aee618696788b462707bb5e4d0ce7694529
kekuwi <132612495+kekuwi@users.noreply.github.com>
2024-06-27 18:49:32 +0700

    List Card Maker Script
    Details:
    made a script to automatically turn json (list) to fully functional card

---
+++ 2D.js
@@ -0,0 +1,132 @@
+let List = {
+    data: [
+        {
+            Title: "3tene",
+            Description: "Freemium",
+            Url: "https://3tene.com/series_purchase/",
+            Image: "https://images.unsplash.com/photo-1715329499545-f57d711c8209?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Animaze",
+            Description: "Subscription",
+            Url: "https://store.steampowered.com/app/1364390/Animaze_by_FaceRig/",
+            Image: "https://images.unsplash.com/photo-1718002127392-92a7eef514ad?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Facerig",
+            Description: "Premium",
+            Url: "https://store.steampowered.com/app/420680/FaceRig_Live2D_Module/",
+            Image: "https://images.unsplash.com/photo-1718012004387-b2ff515edc6d?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "HGFaceDD",
+            Description: "Premium",
+            Url: "https://store.steampowered.com/app/1218010/HGFaceDD/",
+            Image: "https://images.unsplash.com/photo-1718203694879-766b434639f2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "HoloLive",
+            Description: "Free",
+            Url: "https://apps.apple.com/jp/app/%E3%83%9B%E3%83%AD%E3%83%A9%E3%82%A4%E3%83%96-hololive/id1288450594",
+            Image: "https://images.unsplash.com/photo-1718202248233-938f2bced7e1?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Live2DViewerEX",
+            Description: "Premium",
+            Url: "https://store.steampowered.com/app/616720/Live2DViewerEX/",
+            Image: "https://images.unsplash.com/photo-1718301162350-fddcdafa19bc?q=80&w=1439&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Mocape",
+            Description: "Free for PC",
+            Url: "https://note.com/clocknote/n/n0635fc72420e",
+            Image: "https://images.unsplash.com/photo-1718091708959-938f2f241329?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "PrprLive",
+            Description: "Free",
+            Url: "https://store.steampowered.com/app/1279610/PrprLive/",
+            Image: "https://images.unsplash.com/photo-1718102696639-451bd772dca4?q=80&w=1388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Puppermoji",
+            Description: "Free",
+            Url: "https://www.puppemoji.com/",
+            Image: "https://images.unsplash.com/photo-1718267586715-c484519893d5?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "VTMini",
+            Description: "Free",
+            Url: "https://store.steampowered.com/app/1801480/VTmini/",
+            Image: "https://images.unsplash.com/photo-1718049719677-85afb466425a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTh8fHxlbnwwfHx8fHw%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "Vtube Studio",
+            Description: "Freemium",
+            Url: "https://denchisoft.com/",
+            Image: "https://images.unsplash.com/photo-1718031937923-69ec00a67596?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            Tags: "Tracking"
+            },
+            {
+            Title: "VUP",
+            Description: "Free",
+            Url: "https://store.steampowered.com/app/1207050/VUPVTuber_Maker_Animation_MMDLive2D__facial_capture/",
+            Image: "https://images.unsplash.com/photo-1718203695046-0dfd0976a198?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMzl8fHxlbnwwfHx8fHw%3D",
+            Tags: "Tracking"
+            }
+    ],
+};
+
+for (let i of List.data ) {
+    //container
+    let container = document.createElement("a"); 
+        container.href = (i.Url);
+        container.classList.add("card-container", "flex", "flex-column");
+
+    //image container
+    let imgContainer = document.createElement("div");
+        imgContainer.classList.add("image-container");
+        container.appendChild(imgContainer)
+
+    //img image
+    let image = document.createElement("img");
+        image.setAttribute("src", i.Image);
+        image.classList.add("card-image");
+        imgContainer.appendChild(image);
+
+    //text container
+    let cardText = document.createElement("div");
+        cardText.classList.add("card-text");
+        container.appendChild(cardText);
+
+    //Title
+    let name = document.createElement("h3");
+        name.innerText = i.Title.toUpperCase();
+        cardText.appendChild(name);
+
+    //Description
+    let description = document.createElement("p");
+        description.classList.add("description");
+        description.innerText = i.Description;
+        cardText.appendChild(description);
+    
+    document.getElementById("list").appendChild(container);
+}
+
+// input = json
+// make card for each object
+    // check total object on json
+    // make div (base card)
+    // make img (image)
+    // make h3 (title)
+    // make p (description)
